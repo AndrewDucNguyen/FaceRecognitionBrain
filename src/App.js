c1f94884c9c2d4682bfedebd738fa835cda3df62
Andrew Nguyen <anguyen0615@gmail.com>
2023-01-20 08:09:24 -0600

    Implementing Clarifai API
    - Implementing Clarifai face detection API
    - Passed props to components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import {React, Component} from 'react';
 import './App.css';
 import ParticlesBg from 'particles-bg';
-
+import {ClarifaiStub, grpc} from 'clarifai-nodejs-grpc';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 
+const app = new Clarifai.App({
+  apiKey: 'e244becc576241ecaf5169a4c9c444fb'
+});
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="cobweb" bg={true} color='#ffffff'/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      <FaceRecognition />
-    </div>
-  );
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "e244becc576241ecaf5169a4c9c444fb");
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+onInputChange = (e) => {
+  console.log(e.target.value);
+}
+
+onSubmit = () => {
+  
+}
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" bg={true} color='#ffffff'/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
+        <FaceRecognition />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div className='text-white'>
       <p className='text-lg'>Give me an image.</p>
       <div className='flex flex-row w-full justify-center items-center'>
         <div className='p-4 rounded-md shadow-lg w-1/2 flex items-center justify-center'>
-          <input className='w-3/4 p-2' type="text" placeholder='URL' />
-          <button className='w-1/4 px-3 py-2 bg-teal-400'>Detect</button>
+          <input className='w-3/4 p-2 text-black' type="text" placeholder='URL' onChange={onInputChange}/>
+          <button className='w-1/4 px-3 py-2 bg-teal-400' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
